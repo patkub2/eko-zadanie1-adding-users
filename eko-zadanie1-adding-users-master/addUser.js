8e3c6f4ca84bc951233c32188cdaffeca74233d9
Patryk Kubala <patrykkubala1999@gmail.com>
2020-08-10 12:37:29 +0200

    fixed editing elements

---
+++ eko-zadanie1-adding-users-master/addUser.js
@@ -0,0 +1,52 @@
+$("form.send").on("submit", function () {
+  var that = $(this),
+    type = that.attr("method"),
+    data = {};
+  that.find("[name]").each(function (value) {
+    var that = $(this),
+      name = that.attr("name"),
+      value = that.val();
+
+    data[name] = value;
+  });
+
+  $.ajax({
+    url: "http://test.eko.eu/user",
+    type: type,
+    data: data,
+    success: function () {
+      document.getElementById("alert").style.display = "block";
+      document.getElementById("alert").style.color = "green";
+      document.getElementById("alert").style.border = " 2px solid green";
+      document.getElementById("closebtn").style.color = "green";
+      document.getElementById("action").innerHTML = "Added";
+      document.getElementById("button").click();
+      document.getElementById("first_name").value = "";
+      document.getElementById("last_name").value = "";
+      document.getElementById("postal_code").value = "";
+      document.getElementById("street").value = "";
+      document.getElementById("city").value = "";
+      document.getElementById("age").value = "";
+      loadOneUser();
+    },
+  });
+  //////////////////////
+  if (document.getElementById("id_del").value != "") {
+    $.ajax({
+      url: "http://test.eko.eu/user/" + document.getElementById("id_del").value,
+      type: "POST",
+      data: { _method: "delete", id: document.getElementById("id_del").value },
+      success: function () {
+        document.getElementById("button").click();
+        document.getElementById("submit_add").value = "Add";
+        document.getElementById("alert").style.display = "block";
+        document.getElementById("alert").style.color = "orange";
+        document.getElementById("alert").style.border = " 2px solid orange";
+        document.getElementById("closebtn").style.color = "orange";
+        document.getElementById("action").innerHTML = "Edited";
+        loadOneUser();
+      },
+    });
+  }
+  return false;
+});
---
+++ eko-zadanie1-adding-users-master/showOneUser.js
@@ -0,0 +1,147 @@
+document.getElementById("loaduser").addEventListener("click", loadUsers);
+// load users
+function loadUsers() {
+  var userid = document.getElementById("userid").value;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://test.eko.eu", true);
+
+  xhr.onload = function () {
+    if (this.status == 200) {
+      var users = JSON.parse(this.responseText);
+      var outpul = `<tr>
+      <th>First name</th>
+      <th>Last name</th>
+      <th>Postal code</th>
+      <th>Street</th>
+      <th>City</th>
+      <th>Age</th>
+      <th></th>
+      <th></th>
+    </tr>`;
+      id = users.users[userid].id;
+      outpul +=
+        `<td > ` +
+        users.users[userid].first_name +
+        "</td>" +
+        "<td>" +
+        users.users[userid].last_name +
+        "</td>" +
+        "<td>" +
+        users.users[userid].postal_code +
+        "</td>" +
+        "<td>" +
+        users.users[userid].street +
+        "</td>" +
+        "<td>" +
+        users.users[userid].city +
+        "</td>" +
+        "<td>" +
+        users.users[userid].age +
+        "</td>" +
+        //////////////////////////////////
+        `<td><form action="contact.php" method="POST" class="ajax">
+             <input name="_method" type="hidden" value="delete" />
+             <input value="` +
+        users.users[userid].id +
+        `" type="hidden" name="id" />
+
+
+          <button type="submit"  value="edit" class="button" ><img src="./icons/bin.svg" class="icon" alt="no img" /></button>
+         </form></td>` +
+        ////////////////////////////////////////////
+        `<td><form action="edit.php" method="POST" class="ajaxx">
+         <input name="_method" type="hidden" class="table" value="delete" />
+         <input value="` +
+        users.users[userid].id +
+        `" type="hidden" name="id" />
+         <input value="` +
+        users.users[userid].first_name +
+        `" type="hidden" name="first_name" />
+         <input value="` +
+        users.users[userid].last_name +
+        `" type="hidden" name="last_name" />
+         <input value="` +
+        users.users[userid].postal_code +
+        `" type="hidden" name="postal_code" />
+         <input value="` +
+        users.users[userid].street +
+        `" type="hidden" name="street" />
+         <input value="` +
+        users.users[userid].city +
+        `" type="hidden" name="city" />
+         <input value="` +
+        users.users[userid].age +
+        `" type="hidden" name="age" />
+          
+
+
+         <button type="submit"  value="edit" class="button" ><img src="./icons/edit.svg" class="icon" alt="no img" /></button>
+     </form></td>` +
+        "</tr>";
+
+      document.getElementById("users").innerHTML = outpul; // input one user table elements to table users
+    }
+    ////////////   D E L E T E //////////////
+    $("form.ajax").on("submit", function () {
+      var that = $(this),
+        url = that.attr("action"),
+        type = that.attr("method"),
+        data = {};
+      that.find("[name]").each(function (index, value) {
+        var that = $(this),
+          name = that.attr("name"),
+          value = that.val();
+
+        data[name] = value;
+        console.log(data);
+      });
+      //DELETE AJAX//
+      $.ajax({
+        url: "http://test.eko.eu/user/" + data.id,
+        type: type,
+        data: data,
+        success: function (response) {
+          document.getElementById("alert").style.display = "block";
+          document.getElementById("alert").style.color = "red";
+          document.getElementById("alert").style.border = " 2px solid red";
+          document.getElementById("closebtn").style.color = "red";
+          document.getElementById("action").innerHTML = "Deleted";
+          document.getElementById("users").innerHTML = "";
+          loadOneUser();
+        },
+      });
+      return false;
+    });
+    ////////////   E D I T  //////////////
+    $("form.ajaxx").on("submit", function () {
+      var that = $(this),
+        url = that.attr("action"),
+        type = that.attr("method"),
+        data = {};
+      that.find("[name]").each(function (index, value) {
+        var that = $(this),
+          name = that.attr("name"),
+          value = that.val();
+
+        data[name] = value;
+      });
+      //EDIT AJAX//
+      $.ajax({
+        url: "http://test.eko.eu/user/" + data.id,
+        type: type,
+        data: data,
+        success: function (response) {
+          document.getElementById("first_name").value = data.first_name;
+          document.getElementById("last_name").value = data.last_name;
+          document.getElementById("postal_code").value = data.postal_code;
+          document.getElementById("street").value = data.street;
+          document.getElementById("city").value = data.city;
+          document.getElementById("age").value = data.age;
+        },
+      });
+      return false;
+    });
+  };
+
+  xhr.send();
+}
